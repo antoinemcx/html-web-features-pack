20f4a4bcb1b610f9c4e8b3f56a4e4c00b0705b0b
Mélio <47474006+meliooff@users.noreply.github.com>
2023-05-03 14:24:37 +0200

    🎉 New effect : scroll reveal

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 ## html-web-features-pack [![Stars](https://img.shields.io/github/stars/antoinemcx/html-web-features-pack)](https://github.com/antoinemcx/html-web-features-pack) [![Support Server](https://img.shields.io/discord/738122381062832180.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/G6WQsMQShZ)
 
-# 1. Image Carousel
+# 3. Scroll reveal
+Reveal effect while scrolling a page made with vanilla JavaScript, with different transitions delays.
 
 ### Preview
-<img src="https://github.com/antoinemcx/html-web-features-pack/blob/1.-Image-Carousel/preview.jpg">
-
-### Responsive
-This carousel is responsive, but beware : when the screen size decreases, the image size adapts (**object-fit: cover**), so it's up to you if you prefer to do otherwise.
-
-### Non-native tools used
-- Fontawesome (for the previous & next buttons)
+<img src="https://github.com/antoinemcx/html-web-features-pack/blob/3.-Scroll-reveal/preview.gif">
 
 ‎
    
--- js/script.js
+++ js/script.js
@@ -1,40 +1,10 @@
-const img_carousel = document.getElementsByClassName('carousel-img');
-let stage = 0; let img_amount = img_carousel.length;
-const carousel_previous = document.querySelector('.previous');
-const carousel_next = document.querySelector('.next');
-const carousel_pagination = document.querySelectorAll('.carousel_pagination_bubbles span')
-
-function removeCarouselActiveImages() {
-    for(let i=0; i<img_amount ; i++) {
-        img_carousel[i].classList.remove('active');
-        carousel_pagination.forEach(pag => pag.classList.remove('active'));
-    }
-}
-
-function showNextCarouselImage() {
-    stage++;
-    if(stage >= img_amount) { stage = 0; }
-
-    removeCarouselActiveImages();
-    img_carousel[stage].classList.add('active');
-    carousel_pagination[stage].classList.add('active');
-}
-carousel_next.addEventListener('click', function() { showNextCarouselImage() });
-
-carousel_previous.addEventListener('click', function() {
-    stage--;
-    if(stage < 0) { stage = img_amount - 1; }
-
-    removeCarouselActiveImages();
-    img_carousel[stage].classList.add('active');
-    carousel_pagination[stage].classList.add('active');
-});
-
-for(let i=0; i<carousel_pagination.length ; i++) {
-    carousel_pagination[i].addEventListener('click', () => {
-        showNextCarouselImage();
+const handleIntersect = function(entries, observer) {
+    entries.forEach(function(entry) {
+        if(entry.intersectionRatio > .1) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+        }
     })
 }
-
-// This loop will enable auto-scrolling on the carousel
-setInterval(function() { showNextCarouselImage(); }, 4000)
+const observer = new IntersectionObserver(handleIntersect, { root: null, rootMargin: '0px', threshold: .1 });
+document.querySelectorAll('[class*="reveal"]').forEach(function(r) { observer.observe(r) });
